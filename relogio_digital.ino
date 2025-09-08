f912351a6de15b5c95c8bf5a6b0dfb32ba9b2759
danilomelo23 <135890818+danilomelo23@users.noreply.github.com>
2025-09-07 21:48:47 -0300

    Add files via upload

---
+++ relogio_digital.ino
@@ -0,0 +1,236 @@
+const int Seg_A = 7;
+const int Seg_B = 6;
+const int Seg_C = 5;
+const int Seg_D = 4;
+const int Seg_E = 3;
+const int Seg_F = 2;
+const int Seg_G = 8;
+const int H1_K = 14;
+const int H2_K = 15;
+const int M1_K = 16;
+const int M2_K = 17;
+const int S1_K = 18;
+const int S2_K = 19;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+const long interval = 1000;
+int s1 = 0;
+int s2 = 0;
+int h1 = 1;
+int h2 = 7;
+int m1 = 5;
+int m2 = 2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  pinMode(Seg_A, OUTPUT);
+  pinMode(Seg_B, OUTPUT);
+  pinMode(Seg_C, OUTPUT);
+  pinMode(Seg_D, OUTPUT);
+  pinMode(Seg_E, OUTPUT);
+  pinMode(Seg_F, OUTPUT);
+  pinMode(Seg_G, OUTPUT);
+  pinMode(H1_K, OUTPUT);
+  pinMode(H2_K, OUTPUT);
+  pinMode(M1_K, OUTPUT);
+  pinMode(M2_K, OUTPUT);
+  pinMode(S1_K, OUTPUT);
+  pinMode(S2_K, OUTPUT);
+}
+
+void digito(int num) {
+  switch (num) {
+    case 0:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,HIGH);
+      digitalWrite(Seg_F,HIGH);
+      digitalWrite(Seg_G,LOW);
+      break;
+case 1:
+      digitalWrite(Seg_A,LOW);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,LOW);
+      digitalWrite(Seg_E,LOW);
+      digitalWrite(Seg_F,LOW);
+      digitalWrite(Seg_G,LOW);
+      break;
+case 2:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,LOW);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,HIGH);
+      digitalWrite(Seg_F,LOW);
+      digitalWrite(Seg_G,HIGH);
+      break;
+case 3:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,LOW);
+      digitalWrite(Seg_F,LOW);
+      digitalWrite(Seg_G,HIGH);
+      break;
+case 4:
+      digitalWrite(Seg_A,LOW);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,LOW);
+      digitalWrite(Seg_E,LOW);
+      digitalWrite(Seg_F,HIGH);
+      digitalWrite(Seg_G,HIGH);
+      break;
+case 5:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,LOW);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,LOW);
+      digitalWrite(Seg_F,HIGH);
+      digitalWrite(Seg_G,HIGH);
+      break;
+case 6:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,LOW);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,HIGH);
+      digitalWrite(Seg_F,HIGH);
+      digitalWrite(Seg_G,HIGH);
+      break;
+case 7:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,LOW);
+      digitalWrite(Seg_E,LOW);
+      digitalWrite(Seg_F,LOW);
+      digitalWrite(Seg_G,LOW);
+      break;
+case 8:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,HIGH);
+      digitalWrite(Seg_F,HIGH);
+      digitalWrite(Seg_G,HIGH);
+      break;
+case 9:
+      digitalWrite(Seg_A,HIGH);
+      digitalWrite(Seg_B,HIGH);
+      digitalWrite(Seg_C,HIGH);
+      digitalWrite(Seg_D,HIGH);
+      digitalWrite(Seg_E,LOW);
+      digitalWrite(Seg_F,HIGH);
+      digitalWrite(Seg_G,HIGH);
+      break;
+
+  }
+  
+}
+
+
+void loop() {
+
+  currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // salva o tempo atual na variavel previousMillis
+    previousMillis = currentMillis;
+    s2++;
+    if (s2 > 9) {
+      s2 = 0;
+      s1++;
+    }
+    if (s1 > 5) {
+      s1 = 0;
+      m2++;
+    }
+    if (m2 > 9) {
+      m2 = 0;
+      m1++;
+    }
+    if (m1 > 5) {
+      m1 = 0;
+      h2++;
+    }
+    if (h2 > 9) {
+      h2 = 0;
+      h1++;
+    }
+    if ((h1 == 2) && (h2 > 3)) {
+      h1 = 0;
+      h2 = 0;
+    }
+
+  }
+
+  // Envia digito S2
+  digitalWrite(S2_K, LOW);
+  digitalWrite(S1_K, HIGH);
+  digitalWrite(M1_K, HIGH);
+  digitalWrite(M2_K, HIGH);
+  digitalWrite(H1_K, HIGH);
+  digitalWrite(H2_K, HIGH);
+  digito(s2);
+  delay(1);
+
+  // Envia digito S1
+  digitalWrite(S2_K, HIGH);
+  digitalWrite(S1_K, LOW);
+  digitalWrite(M1_K, HIGH);
+  digitalWrite(M2_K, HIGH);
+  digitalWrite(H1_K, HIGH);
+  digitalWrite(H2_K, HIGH);
+  digito(s1);
+  delay(1);
+
+  // Envia digito M2
+  digitalWrite(S2_K, HIGH);
+  digitalWrite(S1_K, HIGH);
+  digitalWrite(M1_K, HIGH);
+  digitalWrite(M2_K, LOW);
+  digitalWrite(H1_K, HIGH);
+  digitalWrite(H2_K, HIGH);
+  digito(m2);
+  delay(1);
+
+  // Envia digito M1
+  digitalWrite(S2_K, HIGH);
+  digitalWrite(S1_K, HIGH);
+  digitalWrite(M1_K, LOW);
+  digitalWrite(M2_K, HIGH);
+  digitalWrite(H1_K, HIGH);
+  digitalWrite(H2_K, HIGH);
+  digito(m1);
+  delay(1);
+
+  // Envia digito H2
+  digitalWrite(S2_K, HIGH);
+  digitalWrite(S1_K, HIGH);
+  digitalWrite(M1_K, HIGH);
+  digitalWrite(M2_K, HIGH);
+  digitalWrite(H1_K, HIGH);
+  digitalWrite(H2_K, LOW);
+  digito(h2);
+  delay(1);
+
+  // Envia digito H1
+  digitalWrite(S2_K, HIGH);
+  digitalWrite(S1_K, HIGH);
+  digitalWrite(M1_K, HIGH);
+  digitalWrite(M2_K, HIGH);
+  digitalWrite(H1_K, LOW);
+  digitalWrite(H2_K, HIGH);
+  digito(h1);
+  delay(1);
+
+
+}
