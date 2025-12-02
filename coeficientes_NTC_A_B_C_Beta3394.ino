46b4edae6c8f9f20a9110f49b3b8b4a996760ac1
danilomelo23 <135890818+danilomelo23@users.noreply.github.com>
2025-12-02 11:39:50 -0300

    Coeficientes A, B e C para NTC 10K com Beta estimado de 3394.40
    Coeficientes A, B e C para NTC 10K com Beta estimado de 3394.40

---
+++ coeficientes_NTC_A_B_C_Beta3394.ino
@@ -0,0 +1,5 @@
+
+// Coeficientes Steinhart-Hart derivados de beta 3394.40
+const float A = 6.4052e-4;    // Coeficiente A
+const float B = 2.9456e-4;    // Coeficiente B
+const float C = 0.0;          // Coeficiente C (zero na aproximacao beta)
