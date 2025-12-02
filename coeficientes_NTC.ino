f52e35a990f7f912e03284c982a953fcdbae0c29
danilomelo23 <135890818+danilomelo23@users.noreply.github.com>
2025-12-02 11:12:11 -0300

    Coeficientes A, B e C para NTC de 10K
    Coeficientes A, B e C para NTC de 10K

---
+++ coeficientes_NTC.ino
@@ -0,0 +1,5 @@
+
+// Coeficientes Steinhart-Hart (versão com A + B*ln(R) + C*(ln(R))^3)
+const float A = 1.12924e-3;   // Coeficiente A
+const float B = 2.34108e-4;   // Coeficiente B
+const float C = 8.7755e-8;    // Coeficiente C (para o termo cubico)
